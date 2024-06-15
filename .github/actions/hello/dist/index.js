32499a0126f3ed36e886a643c4e482b7e2218d53
n-jc <134266319+n-jc@users.noreply.github.com>
2024-06-15 20:52:32 +1000

    private action #4

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -31091,7 +31091,7 @@ try {
 
     const name = core.getInput("who_to_greet");
 
-    console.log('Hello ${name}');
+    console.log('Hello ' + name);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
